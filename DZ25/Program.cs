c278d775c0b0614a1e83e38d3b036aab77c666b9
Sergey Shobuhin <shobuhin@mail.ru>
2022-09-15 11:35:32 +0700

    Задача 25

---
+++ DZ25/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B. 
+// Решить задачу с использованием методов.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16 //// 2*2*2*2 = 16
+
+// вызов функции
+int GetDegree(int a, int b)
+    {
+        int degree = 1;
+        for (int i = 1; i <= b; i++)
+        {
+            degree = degree * a; // умножаем цисло само на себя N раз
+        }
+
+        return degree;    
+    }
+
+
+// Определяем числа для операций
+Console.WriteLine("Введите число которое необходимо возвести в степень: ");
+int numberA = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите показатель степени числа: ");
+int numberB = int.Parse(Console.ReadLine());
+
+int degree = GetDegree(numberA, numberB);
+Console.WriteLine($"Cтепень = {degree}.");
